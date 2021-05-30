532d6e2fb9c43936a2926a9edc1a1edbcf929d77
carlovanbaelen-wico <67262093+carlovanbaelen-wico@users.noreply.github.com>
2021-05-30 16:20:15 +0200

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,32 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    Item = receivedNumber
+    if (Item == 0) {
+        maqueen.motorStop(maqueen.Motors.All)
+    }
+    if (Item == 1) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
+    } else if (Item == 2) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 255)
+    } else if (Item == 3) {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 200)
+    } else if (Item == 4) {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 200)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 0)
+    } else if (Item == 5) {
+        Color.showColor(neopixel.colors(NeoPixelColors.Red))
+    } else if (Item == 6) {
+        maqueen.servoRun(maqueen.Servos.S1, 150)
+    } else if (Item == 7) {
+        Color.showColor(neopixel.colors(NeoPixelColors.Yellow))
+    } else if (Item == 8) {
+        maqueen.servoRun(maqueen.Servos.S1, 10)
+    } else if (Item == 9) {
+        Color.showColor(neopixel.colors(NeoPixelColors.Black))
+    }
+})
+let Item = 0
+let Color: neopixel.Strip = null
+basic.showIcon(IconNames.Happy)
+radio.setGroup(1)
+Color = neopixel.create(DigitalPin.P15, 4, NeoPixelMode.RGB)
