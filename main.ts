a2d9fdd2c365f3f985b96332f17d729f2ed656a7
carlovanbaelen-wico <67262093+carlovanbaelen-wico@users.noreply.github.com>
2021-06-04 14:13:31 +0200

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -8,15 +8,15 @@ radio.onReceivedNumber(function (receivedNumber) {
     } else if (Item == 2) {
         maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 255)
     } else if (Item == 3) {
-        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
-        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 200)
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 0)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 255)
     } else if (Item == 4) {
-        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 200)
-        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 0)
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 255)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 0)
     } else if (Item == 5) {
         Color.showColor(neopixel.colors(NeoPixelColors.Red))
     } else if (Item == 6) {
-        maqueen.servoRun(maqueen.Servos.S1, 60)
+        maqueen.servoRun(maqueen.Servos.S1, 50)
     } else if (Item == 7) {
         Color.showColor(neopixel.colors(NeoPixelColors.Yellow))
     } else if (Item == 8) {
